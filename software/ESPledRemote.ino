a74a58739268d5874f7d9e15a545f57ba1790a0f
S-Biskup <48643069+S-Biskup@users.noreply.github.com>
2019-06-12 18:36:47 +0200

    Update ESPledRemote.ino

--- software/ESPledRemote.ino
+++ software/ESPledRemote.ino
@@ -20,7 +20,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, NeoPIN, NEO_RGB + NEO_KHZ4
 
 
 void setup ( void ) {
-
+ delay(5);
   Serial.begin ( 115200 );
  server.begin();
   WiFi.begin ( ssid, password );
@@ -56,13 +56,79 @@ int c2 = req.indexOf('/',c1+1); //second place
 int red = req.substring(0,c1).toInt();
 int green = req.substring(c1+1,c2).toInt();
 int blue = req.substring(c2+1,c2+4).toInt();
-
+Serial.println(request.substring(10,13));
 Serial.println(request.substring(5,9));
-if(request.substring(5,9)=="true"){
-  for(int i=0; i < NUM_LEDS; i++) {
-    strip.setPixelColor(i, strip.Color( green, red, blue ) );
+if(request.substring(5,9)=="true" && request.substring(10,13)=="an1"){
+  while(1){
+      WiFiClient client = server.available();
+      String request2 = client.readStringUntil('\r');
+  if(request2.substring(10,13)=="an1" || request2.substring(0)==""){
+  for(int j = 0; j < 3; j++ ) { 
+    // Fade IN
+    for(int k = 0; k < 255; k++) {
+      for(int i=0; i < NUM_LEDS; i++) {
+      switch(j) { 
+     case 0: strip.setPixelColor(i, strip.Color( k, 0, 0 ) ); break;
+        case 1: strip.setPixelColor(i, strip.Color( 0, k, 0 ) ); break;
+        case 2: strip.setPixelColor(i, strip.Color( 0, 0, k ) ); break;
+      }
+      }
+      strip.show();
+     delay(5);
+    }
+    // Fade OUT
+    for(int k = 255; k >= 0; k--) { 
+      for(int i=0; i < NUM_LEDS; i++) {
+      switch(j) { 
+        case 0: strip.setPixelColor(i, strip.Color( k, 0, 0 ) ); break;
+        case 1: strip.setPixelColor(i, strip.Color( 0, k, 0 ) ); break;
+        case 2: strip.setPixelColor(i, strip.Color( 0, 0, k ) ); break;
+      }
+      }
+      strip.show();
+       delay(5);
+    }
+    strip.clear();
+  }
+  }
+  else{
+  break;
   }
 }
+}
+  else if(request.substring(5,9)=="true" && request.substring(10,13)=="an2"){
+String req = request.substring(14);
+int ln = req.length()-1;
+int c1 = req.indexOf('/'); //first place to cut string
+int c2 = req.indexOf('/',c1+1); //second place
+int red = req.substring(0,c1).toInt();
+int green = req.substring(c1+1,c2).toInt();
+int blue = req.substring(c2+1,c2+4).toInt();
+while(1){
+   WiFiClient client = server.available();
+      String request2 = client.readStringUntil('\r');
+  if(request2.substring(10,13)=="an2" || request2.substring(0)==""){
+ for(int i=0; i < NUM_LEDS; i++) {
+    
+    strip.setPixelColor(i, strip.Color( green, red, blue ) );
+    strip.setPixelColor(i-1, strip.Color( 0, 0, 0 ) );
+    delay(100);
+    strip.show();
+ }
+ strip.clear();
+}
+else{
+  break;
+}
+}
+  }
+  else if(request.substring(5,9)=="true"){
+  for(int i=0; i < NUM_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color( green, red, blue ) );
+    delay(10);
+  }
+}
+
 else{
   strip.clear();
   }
